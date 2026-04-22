35d95aebf869c258e8556ca9471dd33cd13a275a
TriThuc2321 <trithuc23232@gmail.com>
2026-04-23 00:10:46 +0700

    feat: optimize seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -120,6 +120,20 @@ export default function RootLayout({
     >
       <head>
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RSNC2VRZF6"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RSNC2VRZF6');
+            `,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
         />
